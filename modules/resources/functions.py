3b7ecd0b2c9ee9c698925b092649aa8ff837bbc5
Matt London <mattlondon57@gmail.com>
2020-07-07 01:13:11 -0400

    Add function that checks if something has been run

---
+++ modules/resources/functions.py
@@ -0,0 +1,12 @@
+from . import variables as var
+# This will check if something lives in the list of ran commands
+# Takes a tuple which is strings that the history must contain
+def check(needed):
+    # Loop through needed param
+    for command in needed:
+        if command not in var.bash_history:
+            # If this isn't in it, it must return false
+            return False
+
+    # If everything is in it, it can return true
+    return True
