1dcb0224f9f958236f5b2ef5a2d759e4d19e1bf9
Matt London <mattlondon57@gmail.com>
2020-07-07 22:14:33 -0400

    Build basic filesystem

---
+++ filesystem/Directory.py
@@ -0,0 +1,53 @@
+from resources.colors import colors
+class Directory:
+    # Basic constructor
+    def __init__(self, name="", container=None, contents=[]):
+        self.name = name
+        self.container = container
+        self.contents = contents
+    
+    # grabs index of contents by name
+    def index(self, find):
+        for i in range(len(self.contents)):
+            if self.contents[i].name == find:
+                return i
+        return -1
+    
+    # Add something to the container
+    def add(self, dirFile):
+        # Check for duplicate names
+        if self.index(dirFile.name) != -1:
+            return False
+
+        self.contents.append(dirFile)
+        return True
+
+    # Retrieve something that's in contents
+    def get(self, neededName):
+        ind = self.index(neededName)
+        if ind >= 0:
+            return self.contents[ind]
+        
+            
+
+    # ============== Shell Commands ==============
+
+    # List all contents in directory
+    def ls(self):
+        for content in self.contents:
+            if type(content) == Directory:
+                print(colors.fg.blue + content.name + colors.reset + "\t", end="")
+            else:
+                print(content.name + "\t", end="")
+        print()
+
+    # Delete file by name:
+    def rm(self, toDelete):
+        ind = self.index(toDelete)
+        if ind >= 0:
+            self.contents.remove(ind)
+            return True
+        else:
+            return False
+            
+        
---
+++ filesystem/File.py
@@ -0,0 +1,13 @@
+class File:
+    # Constructor
+    def __init__(self, name="", contents=""):
+        self.name = name
+        self.contents = contents # Contains a string of the file
+
+    # Append to file's text
+    def append(self, text=""):
+        self.contents += text
+    
+    # Returns the text
+    def read(self):
+        return self.contents
