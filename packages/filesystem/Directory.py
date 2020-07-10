1d87e7e8224041de83a1e9ac99baad998bd69622
Matt London <mattlondon57@gmail.com>
2020-07-10 00:12:14 -0400

    Added rm, rmdir, touch, mkdir

--- packages/filesystem/Directory.py
+++ packages/filesystem/Directory.py
@@ -1,5 +1,6 @@
 from ..resources.colors import colors
 from ..resources import variables as var
+from .File import File
 
 class Directory:
     # Basic constructor
@@ -48,37 +49,82 @@ class Directory:
         path = "/".join(pathSplit)
 
         if nextDir >= 0:
+            if type(self.contents[nextDir]) == File:
+                return self.contents[nextDir]
             return self.contents[nextDir].get_sub(path)
         
-        return None
+        print("get_sub error")
 
+    # Delete file/dir by name:
+    def delete(self, path=""):
+        ind = -1
+        if path:
+            pathSplit = path.split("/")
+            if len(pathSplit) == 1:
+                ind = self.index(pathSplit[0])
+                if ind >= 0:
+                    self.contents.pop(ind)
 
-        
-            
+            name = pathSplit.pop()
+            path = "/".join(pathSplit)
+            ind = self.get_sub(path).index(name)
+            if ind >= 0:
+                self.get_sub(path).contents.pop(ind)
+
 
     # ============== Shell Commands ==============
 
     # List all contents in directory
-    def ls(self):
-        for content in self.contents:
+    def ls(self, path=""):
+        destContents = self.get_sub(path).contents
+        lenContent = len(self.get_sub(path).contents)
+
+        for content in destContents:
             if type(content) == Directory:
                 print(colors.fg.blue + content.name + colors.reset + "\t", end="")
             else:
                 print(content.name + "\t", end="")
-        print()
-
-    # Delete file by name:
-    def rm(self, toDelete):
-        ind = self.index(toDelete)
-        if ind >= 0:
-            self.contents.pop(ind)
-            return True
-        else:
-            return False
+        if lenContent != 0:
+            print()
     
     # Make new dir
-    def mkdir(self, name=""):
-        if name:
-            self.add(Directory(name))
-            
-        
+    def mkdir(self, path=""):
+        if path:
+            pathSplit = path.split("/")
+            if len(pathSplit) == 1:
+                self.add(Directory(pathSplit[0]))
+
+            name = pathSplit.pop()
+            path = "/".join(pathSplit)
+            self.get_sub(path).add(Directory(name))
+    
+    # Make new file
+    def touch(self, path=""):
+        if path:
+            pathSplit = path.split("/")
+            if len(pathSplit) == 1:
+                self.add(File(pathSplit[0]))
+
+            name = pathSplit.pop()
+            path = "/".join(pathSplit)
+            self.get_sub(path).add(File(name))
+    
+    # Deletes a file
+    def rm(self, path=""):
+        if path:
+            if type(self.get_sub(path)) == File:
+                self.delete(path)
+            elif type(self.get_sub(path)) == Directory:
+                print("rm error, fed directory")
+            else:
+                print("rm error, Thing doesn't exist")
+
+    # Deletes a directory
+    def rmdir(self, path=""):
+        if path:
+            if type(self.get_sub(path)) == Directory:
+                self.delete(path)
+            elif type(self.get_sub(path)) == File:
+                print("rm error, fed File")
+            else:
+                print("rm error, Thing doesn't exist")
