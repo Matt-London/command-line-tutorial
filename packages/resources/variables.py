9e14dd492199639bb57d85a22bac7e301e3c76c5
Matt London <mattlondon57@gmail.com>
2020-07-21 22:09:31 -0400

    Add more needed global vars

--- packages/resources/variables.py
+++ packages/resources/variables.py
@@ -1,4 +1,5 @@
 from .colors import colors
+
 DEBUG = True # Defines if should run debug utilities
 
 isRoot = False # If the user is root
@@ -10,6 +11,15 @@ shell = "splash" # Name of shell
 ps1 = colors.fg.lightblue + username + colors.fg.darkgrey + "@" + colors.fg.red + hostname + colors.fg.lightcyan + ":" + colors.fg.cyan + "[~/" + wd + "] " + colors.fg.black + "> " + colors.fg.pink + "$#  "[isRoot] + colors.reset # Sets PS1
 
 bash_history = [] # Saves all ran commands
-currentLevel = 1 # contains current levels
+lev_bash_history = [] # All ran commands, cleared after each level
+currentLevel = 0 # contains current level index in levels
 
-args = [] # Contains current args for current command
+banned_commands = [] # List of banned commands
+banned_tokens = [] # List of banned tokens
+
+exit_code = 0 # Contains last exit code
+
+command = "" # Contains current command
+token = [] # Contains current args for current command
+
+workingDir = None # Contains reference to working dir in Interpreter
