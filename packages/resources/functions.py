223afd3a6ae74dd9b6826d03a498e62e8ca70147
Matt London <mattlondon57@gmail.com>
2020-07-22 20:50:56 -0400

    Add clear

--- packages/resources/functions.py
+++ packages/resources/functions.py
@@ -1,4 +1,8 @@
 from . import variables as var
+from os import system, name 
+from ..filesystem.Directory import Directory
+from ..filesystem.File import File
+
 # This will check if something lives in the list of ran commands
 # Takes a tuple which is strings that the history must contain
 def check(needed=()):
@@ -10,3 +14,15 @@ def check(needed=()):
 
     # If everything is in it, it can return true
     return True
+
+# Clears screen
+# CREDIT: https://www.geeksforgeeks.org/clear-screen-python/
+# By: mohit_negi
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
