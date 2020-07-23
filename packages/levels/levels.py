b75637e3edcd81b750e3310acf7b9831cac3a2ca
Matt London <mattlondon57@gmail.com>
2020-07-22 20:50:18 -0400

    Edit level

--- packages/levels/levels.py
+++ packages/levels/levels.py
@@ -1,5 +1,5 @@
 from .Level import Level
 
-one = Level("There's a directory called 'test' within your current working path\nOpen this directory (CHANGE your DIRECTORY to 'thisIsATest')\nThen please print your current working directory to show that you have changed your directory", "You may use the 'cd' command to open a directory\nType 'cd' followed by the name of the directory to open\nYou may type 'pwd' to PRINT your WORKING DIRECTORY", ("cd thisIsATest", "pwd"))
+one = Level("There's a directory called 'test' within your current working path\nOpen this directory (CHANGE your DIRECTORY to 'this')\nThen please print your current working directory to show that you have changed your directory", "You may use the 'cd' command to open a directory\nType 'cd' followed by the name of the directory to open\nYou may type 'pwd' to PRINT your WORKING DIRECTORY", ("cd test", "pwd"))
 
 levels = [one]
