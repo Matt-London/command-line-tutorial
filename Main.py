902301c215e88f5c74e79a6a97c5a0a2645e8a78
Matt London <mattlondon57@gmail.com>
2020-07-24 22:58:16 -0400

    Pre commands for levels

--- Main.py
+++ Main.py
@@ -9,7 +9,16 @@ from packages.parser.Interpreter import Interpreter
 interpreter = Interpreter()
 
 # Put test dir in interpreter
-# interpreter.process("mkdir test")
+interpreter.process("mkdir cdTest")
+interpreter.process("touch cdTest/greatWork")
+interpreter.process("mkdir mvTest")
+interpreter.process("touch mvTest/from")
+
+
+# Clear out these entered commands
+var.lev_bash_history.clear()
+var.bash_history.clear()
+
 
 while True:
     try:
